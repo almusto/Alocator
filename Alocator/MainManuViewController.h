3fc78410e9051a0049b18eb9d1582c8585cb615d
almusto <ales.musto@gmail.com>
2016-06-03 10:55:26 -0400

    Alocator Checkpoint Three

---
+++ Alocator/MainManuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainManuViewController.h
+//  Alocator
+//
+//  Created by Alessandro Musto on 6/3/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainManuViewController : ViewController
+
+@end
---
+++ Alocator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alocator
+//
+//  Created by Alessandro Musto on 6/3/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
